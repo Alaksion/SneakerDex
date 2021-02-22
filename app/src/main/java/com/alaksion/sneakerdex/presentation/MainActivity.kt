dca62494f8df7667a14b14b3c6222f313803682d
Lucca Beurmann <luccab.souza@gmail.com>
2021-02-21 23:08:22 -0300

    changed bottom navigation implentation to work with Navigation Component

--- app/src/main/java/com/alaksion/sneakerdex/presentation/MainActivity.kt
+++ app/src/main/java/com/alaksion/sneakerdex/presentation/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.alaksion.sneakerdex.R
 import com.alaksion.sneakerdex.databinding.ActivityMainBinding
 
@@ -22,34 +24,9 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setUpBottomNavigation() {
-        setUpBottomNavListener()
-        setUpNavigationDefaultPage()
+        val navController = findNavController(R.id.f_nav_host)
+        viewBinding.bnMenu.setupWithNavController(navController)
     }
 
-    private fun setUpNavigationDefaultPage(){
-        viewBinding.bnMenu.selectedItemId = R.id.mi_sneaker_list
-    }
 
-    private fun setUpBottomNavListener() {
-        viewBinding.bnMenu.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.mi_favorites_list -> {
-                    handleFragmentChange(FavoriteSneakersListFragment.getInstance())
-                    true
-                }
-                R.id.mi_sneaker_list -> {
-                    handleFragmentChange(SneakerListFragment.getInstance())
-                    true
-                }
-                else -> false
-            }
-        }
-    }
-
-    private fun handleFragmentChange(frag: Fragment) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(viewBinding.flFragmentHolder.id, frag)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
-    }
 }
